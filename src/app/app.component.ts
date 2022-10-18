f15ebb83eaf36d8dd89f6b625aa77214d5d2cbaf
Christianlopez4 <1107525635@u.icesi.edu.co>
2022-10-18 13:17:26 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Christian';
+  age = 23;
+  img = 'https://images.unsplash.com/photo-1665495655302-b861ceb3f1be?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2NjExNzY1Mg&ixlib=rb-1.2.1&q=80&w=1080';
+
 }
